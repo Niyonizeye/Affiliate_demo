cfe7ba756fd71c64769ff6cd85caac0a13fbcec6
Jean Paul <niyoeanpaul.com>
2021-03-09 17:00:48 +0200

    modal updated

--- resources/views/livewire/front-component.blade.php
+++ resources/views/livewire/front-component.blade.php
@@ -1,3 +1,28 @@
+<style>
+.cover__Text b{
+    position: absolute;
+	left: 0;
+	bottom:0;
+    background: rgb(0, 0, 0); /* Fallback color */
+    background: rgba(0, 0, 0, 0.6); /* Black background with 0.5 opacity */
+    color: #f1f1f1;
+    width: 100%;
+    padding:20px;
+    font-size:20px;
+
+}
+.AcceptableHeader{
+    margin-left:5%;
+    margin-top:2%;
+}
+.well{
+    background-color: blue;
+    opacity:1; 
+}
+.text-uppercase{
+    color:white;
+}
+</style>
 <div>
     {{-- <div id="overlayer"></div>
     <div class="loader">
@@ -13,7 +38,7 @@
                 <div class="col-lg-6">
                     <div class="hero-text">
                         <h1>Join Muhahe Affiliate today</h1>
-                        <p>Here are the best Affiliate website to make money without investements.</p>
+                        <p>START EARNING AS SOON AS YOU SET UP YOUR ACCOUNT WITH US.</p>
                         <a href="#" class="primary-btn">I Want To Hire</a>
                         <a href="#" class="info-btn">I Want To Work</a>
                     </div>
@@ -88,7 +113,16 @@
 
     <!-- row -->
     <div class="slide-row">
-
+    <!-- our services -->
+    <h2 class="text-center">Our services</h2>
+    <div class="d-flex justify-content-around">
+        <div>Click pay</div>
+        <div>Marketing</div>
+        <div>ADVERTISEMENTS</div>
+        <div>VIDEO</div>
+        <div>item2</div>
+    
+    </div>
         <!-- section title -->
         <div class="">
             <div class="sections-title">
@@ -193,10 +227,13 @@
                 <div class="row">
                     <div class="col-md-3">
                         <div class="ap-service-item set-bg" data-setbg="{{asset('assets/front/img/about/about-p1.jpg')}}">
-                            <div class="api-text">
-                                Connect Yourself To The Others Through Muhahe Affiliate
+                            <div class="cover__Text">
+                                    <b>
+                                        Become a stockholder
+                                        You still have a chance to become an Muhahe  stockholder before the products & service  are launched to the public.
+                                    </b>
                             </div>
-                        </div>
+                        </div>                       
                     </div>
                     <!-- STARTING OF NEWS COLUMN -->
                     <div class="col-md-6">
@@ -274,8 +311,6 @@
                     <!-- STARTING OF NEWS COLUMN -->
                     <div class="col-md-3">
                         <div class="ap-service-item set-bg" data-setbg="{{asset('assets/front/img/about/about-p3.jpg')}}">
-                            <div class="api-text">
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -283,8 +318,16 @@
         </div>
     </section>
     <!-- About Us Page Section End -->
-
-    <!-- Video Section Begin -->
+    <!-- service part -->
+    <div class="d-flex justify-content-between">
+        <div>Click pay</div>
+        <div>Click pay</div>
+        <div>Click pay</div>
+        <div>Click pay</div>
+    
+    </div>
+    <!-- end of service div -->
+   <!-- Video Section Begin -->
     <section class="video-section set-bg" data-setbg="{{asset('assets/front/img/video-bg.jpg')}}" style="margin-top: -50px;">
         <div class="container">
             <div class="row">
@@ -300,6 +343,20 @@
         </div>
     </section>
     <!-- Video Section End -->
-
-    <br>
-    <!-- Hero Section End --></div>
+    <!-- payment section -->
+    <div>
+        <h3 class="AcceptableHeader">Acceptable Payment at MUHAHE AFFILIATE.</h3>
+		<marquee behavior="alternate" direction="left">
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/Bitcoin.jpg') }}" width="140" height="100" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/cash.jpg') }}" width="140" height="100" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/etherium.png') }}" width="140" height="100" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/mastercard.jpg') }}" width="147" height="107" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/mtn-mobile-money.png') }}" width="128" height="88" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/PayPal.png') }}" width="120" height="80" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/perfwect.png') }}" width="160" height="120" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/Skrill-Review.png') }}" width="155" height="115" alt="Natural" />
+			<img style="object-fit: contain;" src="{{ asset('assets/front/currency-image/visa.jpeg') }}" width="120" height="80" alt="Natural" />
+		</marquee>
+	</div>
+    <!-- end of payment section -->
+    <!-- Hero Section End --></div
