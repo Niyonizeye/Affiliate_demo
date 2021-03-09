f4ccd1c388b91cf033c3ebb7e83389aec3a0ccd8
Jean Paul <niyoeanpaul.com>
2021-03-09 20:42:04 +0200

    The slide photo is now uploaded

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\ContactComponent;
 use App\Http\Livewire\User\UserPackageComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
-use App\Http\Livewire\ContactComponent;
 
 /*
 |--------------------------------------------------------------------------
